b72f2d345b3841aca8f22589ff347ec471dc91bf
dungpham-programming <21130326@st.hcmuaf.edu.vn>
2024-07-02 13:43:14 +0700

    Thêm implement cho các còn thiếu

--- app/src/main/java/com/mue/music/model/Artist.java
+++ app/src/main/java/com/mue/music/model/Artist.java
@@ -37,4 +37,9 @@ public class Artist implements CardItem {
     public String getSubtitle() {
         return "";
     }
+
+    @Override
+    public String getImageUrl() {
+        return "";
+    }
 }
